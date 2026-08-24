35ab99be377e9897bba99cf7023b4c802ac2252b
Jeeva-Udemy <jvaudemy@gmail.com>
2026-08-24 16:19:10 +0530

    Fix build: build failure fixed

--- app/src/main/java/com/example/cricketscorer/ui/SquadScreen.kt
+++ app/src/main/java/com/example/cricketscorer/ui/SquadScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.relocation.BringIntoViewRequester
 import androidx.compose.foundation.relocation.bringIntoViewRequester
 import androidx.compose.material.icons.Icons
@@ -196,6 +197,7 @@ fun SquadScreen(
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun SquadCard(
     squad: SquadEntity,
